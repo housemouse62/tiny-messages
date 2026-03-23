f3b342c91a70eb39731993a9d394e3ac37ecb652
housemouse62 <ryan.skeels@gmail.com>
2026-03-23 23:38:47 +0100

    render with basic name & message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,7 +3,7 @@ import db from "../db/queries.js";
 async function createMessageGet(req, res) {
   const messages = await db.getAllMessages();
   console.log(messages);
-  //   res.render("index", { title: "Tiny Messageboard", messages: messages });
+  res.render("index", { title: "Tiny Messageboard", messages: messages });
 }
 
 export { createMessageGet };
